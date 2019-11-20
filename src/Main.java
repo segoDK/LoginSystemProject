581f57949f88ba81cf286e7e276032250d7b9565
Sebastian <sego@live.dk>
2019-11-20 10:31:49 +0100

    Little note added

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main{
 
 
 
-    public static void main(String[] args){
-        System.out.println("HelloWord");
+    public static void main(String[] args) {
+        Screen.setup();
     }
+
+
+    /* Opsætning af projektet:
+     *
+     * Vi skal lave en class for Users
+     * Der skal sættes noget algoritme op til at kryptere password
+     * Passwords og burgerinfo skal kunne gemmes
+     * GUI skal sættes op
+    */
+
 }
--- src/Screen.java
+++ src/Screen.java
@@ -1,5 +1,19 @@
-public class Screen {
+import javax.swing.*;
+import java.awt.*;
 
-    
+public class Screen {
+    private JTextField userNameField;
+    private JPasswordField passwordField;
+    private JLabel startupText;
+    private JPanel panelMain;
+
+    public static void setup(){
+        JFrame frame = new JFrame("FirstForm");
+        frame.setSize(new Dimension(920, 540));
+        frame.setContentPane(new Screen().panelMain);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //frame.pack();
+        frame.setVisible(true);
+    }
 
 }
