90d389485bcb2d719d263b016d05a0df896a5ebb
Sebastian <sego@live.dk>
2019-11-20 11:32:06 +0100

    Add some listeners and variables to the GUI

--- src/Screen.java
+++ src/Screen.java
@@ -1,11 +1,37 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.print.Book;
 
 public class Screen {
     private JTextField userNameField;
     private JPasswordField passwordField;
     private JLabel startupText;
     private JPanel panelMain;
+    private JButton loginButton;
+
+    private final static String newline = "\n";
+    private static String username;
+    private static String password;
+
+    public Screen() {
+        userNameField.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                username = userNameField.getText();
+                //userNameField.selectAll();
+            }
+        });
+        passwordField.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                passwordField.selectAll();
+                password = passwordField.getSelectedText();
+
+            }
+        });
+    }
 
     public static void setup(){
         JFrame frame = new JFrame("FirstForm");
